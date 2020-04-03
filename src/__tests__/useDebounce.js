12372cc3d860e93e6f955c2ac75398e82b661715
Tony <arinaldi@vt.edu>
2020-04-03 15:21:59 -0500

    Trim input in useDebounce hook

--- src/__tests__/useDebounce.js
+++ src/__tests__/useDebounce.js
@@ -1,10 +1,24 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { act, renderHook } from '@testing-library/react-hooks';
 
 import useDebounce from '../hooks/useDebounce';
 
-test('useDebounce handles value input', () => {
+test('useDebounce handles value input', async () => {
   const value = 'test';
-  const { result } = renderHook(() => useDebounce(value));
+  const { result } = renderHook(() => useDebounce(value, 0));
 
   expect(result.current).toEqual(value);
+});
+
+test('useDebounce returns trimmed string', async () => {
+  const value = 'test   ';
+  const { result, waitForNextUpdate } = renderHook(() => useDebounce(value, 0));
+
+  setImmediate(() => {
+    act(() => {
+      jest.useFakeTimers();
+    });
+  });
+
+  await waitForNextUpdate();
+  expect(result.current).toEqual(value.trim());
 });
--- src/hooks/useDebounce.js
+++ src/hooks/useDebounce.js
@@ -5,7 +5,7 @@ const useDebounce = (value, delay = 500) => {
 
   useEffect(() => {
     const handler = setTimeout(() => {
-      setDebouncedValue(value);
+      setDebouncedValue(value.trim());
     }, delay);
 
     return () => {
