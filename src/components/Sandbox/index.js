1a8c48c5cafd56a26553a9a7c689156934d85a8b
Tony <arinaldi@vt.edu>
2020-09-11 10:06:30 -0500

    Formatting updates

--- src/components/Sandbox/index.js
+++ src/components/Sandbox/index.js
@@ -1,16 +1,11 @@
-import React, { useReducer } from 'react';
+import React, { useReducer } from "react";
 
-import Sandbox from './presenter';
+import Sandbox from "./presenter";
 
 const SandboxContainer = () => {
-  const [page, setPage] = useReducer(page => page + 1, 1);
+  const [page, setPage] = useReducer((page) => page + 1, 1);
 
-  return (
-    <Sandbox
-      handleNext={setPage}
-      page={page}
-    />
-  );
+  return <Sandbox handleNext={setPage} page={page} />;
 };
 
 export default SandboxContainer;
