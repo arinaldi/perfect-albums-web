9bda76da25d6753c6b929d67008304169d71753b
Tony <arinaldi@vt.edu>
2021-07-28 14:23:26 -0500

    Upgrade packages

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,8 @@ import loadVersion from 'vite-plugin-package-version';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    chunkSizeWarningLimit: 700,
+  },
   plugins: [reactJsx(), reactRefresh(), loadVersion()],
 });
